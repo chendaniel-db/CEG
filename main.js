b9507982b796c94ec3af0bed1d6a158a182d821c
Daniel Chen <chen.0910705@gmail.com>
2023-06-06 01:02:32 +0800

    UPDATE202306060102

--- main.js
+++ main.js
@@ -24,3 +24,18 @@
 	});
 
 })(jQuery);
+
+$.getJSON("https://ipinfo.io", function (response) {
+    $("#mycity").html(response.city);
+    $("#mycountry").html(response.country);
+    $("#myip").html(response.ip);
+    $("#mytimezone").html(response.timezone);
+})
+
+function ShowTime() {
+    var NowDate = new Date();
+    var d = NowDate.getDay();
+    var dayNames = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    document.getElementById('showbox').innerHTML = '系統時間：' + NowDate.toLocaleString() + '（ ' + dayNames[d] + ' ）'; 
+    setTimeout('ShowTime()', 1); 
+}
