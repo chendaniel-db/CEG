5afc140a8818ce90d9c68bfe13775348ec626e88
Daniel Chen <54165457+chendaniel-db@users.noreply.github.com>
2023-06-13 20:59:16 +0800

    Update main.js

--- main.js
+++ main.js
@@ -36,6 +36,6 @@ function ShowTime() {
     var NowDate = new Date();
     var d = NowDate.getDay();
     var dayNames = new Array("星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
-    document.getElementById('showbox').innerHTML = '系統時間：' + NowDate.toLocaleString() + '（ ' + dayNames[d] + ' ）'; 
+    document.getElementById('showbox').innerHTML = '系統時間：' + NowDate.toLocaleString() + '（' + dayNames[d] + '）'; 
     setTimeout('ShowTime()', 1); 
 }
